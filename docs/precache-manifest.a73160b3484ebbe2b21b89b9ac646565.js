7133d61b8ebeff1b296fd8a0efa0600377c80f9c
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2020-02-03 21:51:55 +0530

    docs

---
+++ docs/precache-manifest.a73160b3484ebbe2b21b89b9ac646565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde623418da79ce34e7f37617ddf14e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "918604ab461bc36ecedf",
+    "url": "/static/css/main.17e5c31d.chunk.css"
+  },
+  {
+    "revision": "8cffca48ad9d7c7a2a50",
+    "url": "/static/js/2.de90376e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.de90376e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "918604ab461bc36ecedf",
+    "url": "/static/js/main.cfcbdef2.chunk.js"
+  },
+  {
+    "revision": "f2194a12aafa68697fd9",
+    "url": "/static/js/runtime-main.ae7076f4.js"
+  }
+]);
